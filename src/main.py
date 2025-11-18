26f00d7a1ab6a98b308f7a62adb7502ab2def246
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-19 00:47:54 +0530

    git commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
+import os
 from src.extract.tmdb_extract import fetch_popular_pages
 from src.transform.transform_movies import transform_movies
 from src.load.load_to_postgres import upsert_movies
 from config.config import MAX_PAGES, SCHEDULE
 from src.utils.logger import get_logger
-import time
 
 logger = get_logger('main')
 
@@ -16,21 +16,18 @@ def run_once():
     logger.info("=== Pipeline run completed ===")
 
 
-def run_loop(interval):
-    while True:
-        run_once()
-        logger.info(f"Sleeping for {interval} seconds before next run...")
-        time.sleep(interval)
-
-
 if __name__ == "__main__":
-    try:
-        interval = int(SCHEDULE)
-    except:
-        interval = 0
-
-    if interval > 0:
-        run_loop(interval)
-    else:
+    # Force CI to run ONLY ONCE
+    if os.environ.get("GITHUB_ACTIONS") == "true":
         run_once()
+    else:
+        # Local mode
+        if SCHEDULE and int(SCHEDULE) > 0:
+            import time
+            while True:
+                run_once()
+                logger.info(f"Sleeping for {SCHEDULE} seconds...")
+                time.sleep(int(SCHEDULE))
+        else:
+            run_once()
 
