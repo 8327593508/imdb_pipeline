42416967393dd706858554cbf573710c057620e4
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 19:54:56 +0530

    Update db_engine.py

--- src/utils/db_engine.py
+++ src/utils/db_engine.py
@@ -4,3 +4,6 @@ from config.config import PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DB
 def get_engine():
     url = f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}?sslmode=disable"
     return create_engine(url)
+
+# Global engine instance used by loaders
+engine = get_engine()
