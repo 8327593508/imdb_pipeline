bcc7ff7765e3154a1b0dcba7c51dba8d1babf83b
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 22:41:01 +0530

    Update load_movie_details.py

--- src/load/load_movie_details.py
+++ src/load/load_movie_details.py
@@ -1,7 +1,35 @@
-import pandas as pd
 from src.utils.db_engine import get_engine
-engine = get_engine()
+from src.utils.logger import get_logger
 
-def upsert_movie_details(df: pd.DataFrame):
-    df.to_sql("movie_details", engine, if_exists="append", index=False)
+logger = get_logger("load_movie_details")
 
+
+def upsert_movie_details(df_details):
+    engine = get_engine()
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+
+    for _, row in df_details.iterrows():   # FIXED
+
+        values = (
+            int(row["movie_id"]),
+            row["genres"],
+            row["runtime"],
+            row["budget"],
+            row["revenue"]
+        )
+
+        cursor.execute("""
+            INSERT INTO movie_details (movie_id, genres, runtime, budget, revenue)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (movie_id)
+            DO UPDATE SET
+                genres = EXCLUDED.genres,
+                runtime = EXCLUDED.runtime,
+                budget = EXCLUDED.budget,
+                revenue = EXCLUDED.revenue;
+        """, values)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
