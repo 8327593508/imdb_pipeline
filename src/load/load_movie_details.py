e5735e9173161b00c9dfea141a21096031d59439
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 22:06:18 +0530

    Update load_movie_details.py

--- src/load/load_movie_details.py
+++ src/load/load_movie_details.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from src.utils.db_engine import engine
+from src.utils.db_engine import get_engine
+engine = get_engine()
 
 def upsert_movie_details(df: pd.DataFrame):
     df.to_sql("movie_details", engine, if_exists="append", index=False)
