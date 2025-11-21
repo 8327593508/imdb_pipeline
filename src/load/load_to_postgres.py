4068a9e9550d55b4a17225b022a2758f7f8912d3
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 21:57:07 +0530

    Update load_to_postgres.py

--- src/load/load_to_postgres.py
+++ src/load/load_to_postgres.py
@@ -1,7 +1,25 @@
-import pandas as pd
 from src.utils.db_engine import get_engine
 
-def upsert_movies(df: pd.DataFrame):
-    engine=get_engine()
-    df.to_sql("popular_movies", engine, if_exists="append", index=False)
+def upsert_movies(movies):
+    engine = get_engine()
+    with engine.begin() as conn:
+        for movie in movies:
+            conn.execute(
+                """
+                INSERT INTO movies (movie_id, title, popularity, vote_average)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT (movie_id)
+                DO UPDATE SET
+                    title = EXCLUDED.title,
+                    popularity = EXCLUDED.popularity,
+                    vote_average = EXCLUDED.vote_average;
+                """,
+                (
+                    movie["movie_id"],
+                    movie["title"],
+                    movie["popularity"],
+                    movie["vote_average"]
+                )
+            )
+
 
