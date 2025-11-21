2a033a1926ddfe2ac9282891b52e7b5fe0d326e9
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 21:43:19 +0530

    Update db_engine.py

--- src/utils/db_engine.py
+++ src/utils/db_engine.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
-from config.config import PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DB
+from config.config import PG_USER, PG_PASSWORD, PG_PORT, PG_DB
 
 def get_engine():
-    url = f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}?sslmode=disable"
+    url = f"postgresql+psycopg2://{PG_PASSWORD}@postgres:{PG_PORT}/{PG_DB}?sslmode=disable"
     return create_engine(url)
 
 # Global engine instance used by loaders
