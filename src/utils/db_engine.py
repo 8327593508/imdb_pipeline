fe60a723192a668ed3a25140cc5bba571bb9421a
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 21:51:17 +0530

    Update db_engine.py

--- src/utils/db_engine.py
+++ src/utils/db_engine.py
@@ -3,9 +3,5 @@ from config.config import PG_USER, PG_PASSWORD, PG_PORT, PG_DB
 
 
 def get_engine():
-    """
-    Creates a SQLAlchemy engine connected to the Postgres container.
-    Hostname 'postgres' matches docker-compose service name.
-    """
     url = f"postgresql://{PG_USER}:{PG_PASSWORD}@postgres:{PG_PORT}/{PG_DB}"
     return create_engine(url)
