e7c797c8312215a2a19eebd415ce55b63c1cb3a9
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-22 01:05:29 +0530

    Update transform_movies.py

--- src/transform/transform_movies.py
+++ src/transform/transform_movies.py
@@ -1,5 +1,29 @@
+# src/transform/transform_movies.py
+
 import pandas as pd
+from src.utils.logger import get_logger
 
-def transform_movies(raw_movies):
-    return pd.DataFrame(raw_movies)
+logger = get_logger("transform_movies")
 
+
+def transform_movies(rows):
+    logger.info(f"Transforming {len(rows)} popular movies...")
+
+    # TMDB returns 'id', 'title', 'vote_average', etc.
+    df = pd.DataFrame(rows)
+
+    # Keep only the columns you need
+    expected_cols = [
+        "id",
+        "title",
+        "vote_average",
+        "vote_count",
+        "popularity",
+        "release_date",
+        "original_language",
+    ]
+    df = df[expected_cols]
+
+    df["release_date"] = pd.to_datetime(df["release_date"], errors="coerce")
+
+    return df
