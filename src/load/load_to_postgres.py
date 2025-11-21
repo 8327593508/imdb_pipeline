342932306b8488b8f757ea47d0766cdacd84a648
Subham Das <67760553+8327593508@users.noreply.github.com>
2025-11-21 21:54:01 +0530

    Update load_to_postgres.py

--- src/load/load_to_postgres.py
+++ src/load/load_to_postgres.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from src.utils.db_engine import engine
+from src.utils.db_engine import get_engine
 
 def upsert_movies(df: pd.DataFrame):
+    engine=get_engine()
     df.to_sql("popular_movies", engine, if_exists="append", index=False)
 
